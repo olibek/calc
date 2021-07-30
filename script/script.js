1a1d09f5a254d4f292610db1088279d69aa53d40
Alibek Botashev <botashev_alibek@mail.ru>
2021-07-30 11:52:51 +0300

    add lesson 2

--- script/script.js
+++ script/script.js
@@ -1,9 +1,22 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 30000;
+let income = 3000;
+let addExpenses = 'Интернет, Топливо, Сигареты, Комуналка';
+let deposit = true;
+let mission = 500000;
+let period = 8;
 
 alert('Привет Мир!');
-console.log('Привет мир!')
+console.log('Привет мир!');
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей!');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay;
+budgetDay = (money + income) / 30;
+console.log(budgetDay);
