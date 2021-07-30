96eee552562ce1f1af3d210bab475597a7caa219
Alibek Botashev <botashev_alibek@mail.ru>
2021-07-30 15:29:53 +0300

    fix(script.js): fix all

--- script/script.js
+++ script/script.js
@@ -1,22 +1,19 @@
 let money = 30000;
-let income = 3000;
+let income = 'фриланс';
 let addExpenses = 'Интернет, Топливо, Сигареты, Комуналка';
 let deposit = true;
 let mission = 500000;
 let period = 8;
+let budgetDay;
+
+budgetDay = money / 30;
 
 alert('Привет Мир!');
-console.log('Привет мир!');
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log('Привет мир!');
+console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей!');
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
-
-let budgetDay;
-budgetDay = (money + income) / 30;
+console.log(addExpenses.toLowerCase().split(', '));
 console.log(budgetDay);
